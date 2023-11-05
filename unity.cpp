675478114e439b7644155653586fda7c2dd3c991
Artem62512 <144590995+Artem62512@users.noreply.github.com>
2023-11-05 12:39:32 +0300

    Unity.cpp

--- unity.cpp
+++ unity.cpp
@@ -34,14 +34,15 @@ float line(int x)
 
 float main()
 {
-    const int len = 30, strings = 19;
+    // создаем массив на 19 строк, в каждой строке которого 30 знаков
+    const int len = 30, strings = 19; 
     const char ch = '\n';
     char mass[len][strings];
     int x = 0;
 
     ofstream fout("lab1.txt"); // создание текстового файла
 
-    for (int x = -10; x <= 8; x++)
+    for (int x = -10; x <= 8; x++) // цикл записи значений "x" и "y" в текстовый файл
     {
         fout << "x = " << x << "   y = " << line(x) << endl; // запись значений в файл
     }
